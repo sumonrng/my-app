a10f19bc301a6d1d84c2cc0e247669a074a87dc9
sumonrng <63655404+sumonrng@users.noreply.github.com>
2022-02-21 00:58:09 +0600

    my-app

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App() {
+  const description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={styles.homeTop}>
+        <Image
+          style={styles.headerImage}
+          resizeMode="contain"
+          source={require("./assets/header.jpg")}
+        />
+        <Text style={styles.mainHeader}>Welcome to </Text>
+        <Text style={[styles.mainHeader,{
+          fontSize: 33,
+          color:"#4c5dab"
+        }]}> Samiul Islam</Text>
+        <Text style={styles.paraStyle}>{description}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  mainContainer: {
+    height:"100%",
+    display:"flex",
+    justifyContent:"space-between",
+    paddingHorizontal:20,
+    backgroundColor:"#fff",
+    textAlign:"center",
+
   },
+  homeTop:{
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    paddingHorizontal:10,
+    
+  },
+  headerImage:{
+    height:undefined,
+    width:"100%",
+    aspectRatio:1,
+    marginTop:20,
+    borderRadius:20,
+  },
+  mainHeader:{
+    fontSize:30,
+    color:"#344055",
+    textTransform:"uppercase",
+  },
+  paraStyle:{
+    textAlign:"left",
+    fontSize:19,
+    color:"#7d7d7d",
+    marginTop:30,
+    paddingBottom:50,
+    lineHeight:26,
+  }
 });
---
+++ src/screens/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const Home = () => {
+    return(
+        <View>
+            <Text>
+                Hello World
+            </Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+});
+export default Home;
